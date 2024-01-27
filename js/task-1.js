0aa21f993722c177b1bee4f8544988dd7f2f7134
Miteos1986 <mdmitriy1986@gmail.com>
2024-01-27 19:25:04 +0100

    исправлено.
    Удалено

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,8 @@
-let quantity = 5;
-
-let pricePerDroid = 3000;
-
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-makeTransaction();
+
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
 console.log(makeTransaction(10, 500));
