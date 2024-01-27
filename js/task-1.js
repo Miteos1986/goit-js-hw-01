0085d0a9cd2e5ba3a2efbd53a25e4570c45ae48a
Miteos1986 <mdmitriy1986@gmail.com>
2024-01-27 15:36:22 +0100

    update task - 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,12 @@
 let quantity = 5;
-// quantity = 3;
-// quantity = 10;
+
 let pricePerDroid = 3000;
-// pricePerDroid = 1000;
-// pricePerDroid = 500;
-totalPrice = quantity * pricePerDroid;
-function makeTransaction(quantity, totalPrice) {
+
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-
 makeTransaction();
-
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
